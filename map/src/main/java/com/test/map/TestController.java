4d8e63c4fe9b201934370fc1f571c0221ffdca7f
kamesh1014 <48281906+kamesh1014@users.noreply.github.com>
2020-01-28 13:29:45 +0530

    Update TestController.java

--- map/src/main/java/com/test/map/TestController.java
+++ map/src/main/java/com/test/map/TestController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.maxmind.geoip.Location;
 import com.maxmind.geoip.LookupService;
